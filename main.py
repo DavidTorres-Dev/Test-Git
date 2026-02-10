019c797da9204d6c1a3e7bd1d71257552be10dc0
DavidDevBio <dalmeida@stackmaster.ec>
2026-02-10 11:14:46 -0500

    Version 1.0.0

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ print("Hola mundo")
 nombre="david"
 def saludar(nombre):
     print(f"Hola {nombre}")
+print("Esta es la ultima prueba para aprender todos los cambios")
+print("Esto quedara marcado en el tiempo y un dia despues de años me dare cuenta como todo empezo")
